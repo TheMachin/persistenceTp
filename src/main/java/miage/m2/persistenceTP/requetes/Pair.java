d0cec9dd70341d3799155a18d1e9d850bfb60037
TheMachin <thibaultmartinec@gmail.com>
2017-10-24 00:12:32 +0200

    update request pair

--- src/main/java/miage/m2/persistenceTP/requetes/Pair.java
+++ src/main/java/miage/m2/persistenceTP/requetes/Pair.java
@@ -34,12 +34,12 @@ public class Pair {
 		Query q = em.createQuery(r);
 		q.setParameter("over", false);
 		List<Projet> listP = q.getResultList();
-		System.out.println("Liste des projets en cours : ");
+		System.out.println("2. Liste des projets en cours : ");
 		if(listP.isEmpty()){
 			System.out.println("Aucun projet en cours");
 		}
 		for(Projet p : listP){
-			System.out.println(p.toString());
+			System.out.println(p.getRefProjet());
 		}
 		
 	}
@@ -49,7 +49,7 @@ public class Pair {
 		Query q = em.createQuery(r);
 		q.setParameter("over", false);
 		List<Projet> listP = q.getResultList();
-		System.out.println("Nombre de projets réalisé portant sur un établissement scolaire : "+listP.size());
+		System.out.println("4. Nombre de projets réalisé portant sur un établissement scolaire : "+listP.size());
 	}
 	
 	public void nomContactEntrepriseGeneralBatiment_6(){
@@ -57,7 +57,7 @@ public class Pair {
 		Query q = em.createQuery(r);
 		q.setParameter("name", "Général Bâtiment");
 		List<Acteur> list = q.getResultList();
-		System.out.println("Nom des contacts de l'entreprise Général Bâtiment");
+		System.out.println("6. Nom des contacts de l'entreprise Général Bâtiment");
 		if(list.isEmpty()){
 			System.out.println("Aucun contact");
 		}
@@ -66,31 +66,29 @@ public class Pair {
 		}
 	}
 	
-	/**
-	 * Voir si ça marche
-	 */
 	public void lotsProjetEnCoursQueParticipeEntrepriseGeneralBatiment_8(){
 		
-		String r = "Select l FROM Projet p JOIN p.lots l JOIN l.realisateurs e WHERE e.nom=:name AND p.termine=:over";
+		String r = "Select DISTINCT l FROM Projet p JOIN p.lots l JOIN l.realisateurs e JOIN l.responsable r "
+				+ "WHERE ((e.nom=:name AND r.nom=:name) OR (e.nom=:name) OR (r.nom=:name)) AND p.termine=:over";
 		Query q = em.createQuery(r);
 		q.setParameter("name", "Général Bâtiment");
 		q.setParameter("over", false);
-		List<Lot> list = q.getResultList();
-		System.out.println("Lots de projet en cours que l'entreprise Général Bâtiment participe");
+		List<Object> list = q.getResultList();
+		System.out.println("8. Lots de projet en cours que l'entreprise Général Bâtiment participe");
 		if(list.isEmpty()){
 			System.out.println("Elle ne participe à aucun projet en cours");
 		}
-		for(Lot l : list){
+		for(Object l : list){
 			System.out.println(l.getClass().getSimpleName());
 		}
 	}
 	
 	public void nbLotsProjetPLot12_10(){
-		String r = "Select p FROM Projet p WHERE p.refProjet=:ref";
+		String r = "Select p.lots FROM Projet p WHERE p.refProjet=:ref";
 		Query q = em.createQuery(r);
 		q.setParameter("ref", "PLot12");
 		int nbResult = q.getResultList().size();
-		System.out.println("Nombre de lots du projet PLot12 : "+nbResult);
+		System.out.println("10. Nombre de lots du projet PLot12 : "+nbResult);
 	}
 	
 	public void entrepriseEtAdresseQuiOntRealiseMenuiseriesDansProjetDeMusee_12(){
@@ -99,7 +97,10 @@ public class Pair {
 		Query q = em.createQuery(r);
 		q.setParameter("type", Menuiseries.class);
 		List<Entreprise> list = q.getResultList();
-		System.out.println("Entreprise de qui ont réalisées les menuiseries dans les projet de Musée");
+		System.out.println("12. Entreprise de qui ont réalisées les menuiseries dans les projet de Musée");
+		if(list.isEmpty()){
+			System.out.println("Pas de menuiseries réalisées");
+		}
 		for(Entreprise e : list){
 			System.out.println("Nom entreprise : "+e.getNom()+" "+e.getSiegeSocial().toString());
 		}
@@ -110,12 +111,12 @@ public class Pair {
 		Query q = em.createQuery(r);
 		q.setParameter("ref", "PLot12");
 		List<Object> list = q.getResultList();
-		System.out.println("Avancement des lots du projet PLot12");
+		System.out.println("14. Avancement des lots du projet PLot12");
 		if(list.isEmpty()){
 			System.out.println("Aucun lot dans le projet");
 		}
 		for(Object s : list){
-			System.out.println("Type du lot : "+s.getClass().getSimpleName());
+			System.out.println("Type du lot : "+s.getClass().getSimpleName()+" Avancement : "+((Lot) s).getAvancement());
 		}
 	}
 	
